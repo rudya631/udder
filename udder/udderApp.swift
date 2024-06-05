080edb10e5e8f0370ee1b89aebd8270499c3d2da
Rudy Arnold <rudyarnold@gmail.com>
2024-06-05 07:18:09 -0400

    Initial Commit

---
+++ udder/udderApp.swift
@@ -0,0 +1,17 @@
+//
+//  udderApp.swift
+//  udder
+//
+//  Created by The Arnolds on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct udderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
